330ba332c19cb5e0e6a532cba53f0528f536ba12
Lionel Green <lgree73@lsu.edu>
2025-08-30 15:57:46 -0500

    I'm Done!!!

---
+++ sketch.js
@@ -0,0 +1,244 @@
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+    background(0,0,0);
+    noStroke();
+    
+    fill(255);
+    ellipse(300,300,50,560);
+
+    fill(255);
+    ellipse(300,300,560,50);
+
+    fill(0);
+    ellipse(300,300,100,100);
+
+    //Top Left
+    fill(255);
+    ellipse(10,10,580,580);
+
+    fill(0);
+    ellipse(10,10,530,530);
+
+    fill(255);
+    ellipse(10,10,500,500);
+
+    fill(0);
+    ellipse(10,10,480,480);
+
+    fill(255);
+    ellipse(10,10,430,430);
+
+    fill(0);
+    ellipse(10,10,390,390);
+
+    fill(255);
+    ellipse(10,10,360,360);
+
+    fill(0);
+    ellipse(10,10,340,340);
+
+    fill(255);
+    ellipse(10,10,300,300);
+
+    fill(0);
+    ellipse(10,10,250,250);
+
+    fill(255);
+    ellipse(10,10,200,200);
+
+    fill(0);
+    ellipse(10,10,180,180);
+
+    fill(255);
+    ellipse(10,10,150,150);
+
+    fill(255);
+    ellipse(10,10,100,100);
+
+    //Top Right
+    fill(255);
+    ellipse(300,300,50,500);
+
+    fill(255);
+    ellipse(300,300,500,50);
+
+    fill(0);
+    ellipse(300,300,100,100);
+
+    //Top Left
+    fill(255);
+    ellipse(590,10,580,580);
+
+    fill(0);
+    ellipse(590,10,530,530);
+
+    fill(255);
+    ellipse(590,10,500,500);
+
+    fill(0);
+    ellipse(590,10,480,480);
+    
+    fill(255);
+    ellipse(590,10,430,430);
+
+    fill(0);
+    ellipse(590,10,390,390);
+
+    fill(255);
+    ellipse(590,10,360,360);
+
+    fill(0);
+    ellipse(590,10,340,340);
+
+    fill(255);
+    ellipse(590,10,300,300);
+
+    fill(0);
+    ellipse(590,10,250,250);
+
+    fill(255);
+    ellipse(590,10,200,200);
+
+    fill(0);
+    ellipse(590,10,180,180);
+
+    fill(255);
+    ellipse(590,10,150,150);
+    
+    //Bottom Left
+    fill(255);
+    ellipse(300,300,50,500);
+
+    fill(255);
+    ellipse(300,300,500,50);
+
+    fill(0);
+    ellipse(300,300,100,100);
+
+    //Top Left
+    fill(255);
+    ellipse(10,590,580,580);
+
+    fill(0);
+    ellipse(10,590,530,530);
+
+    fill(255);
+    ellipse(10,590,500,500);
+
+    fill(0);
+    ellipse(10,590,480,480);
+
+    fill(255);
+    ellipse(10,590,430,430);
+    
+    fill(0);
+    ellipse(10,590,390,390);
+
+    fill(255);
+    ellipse(10,590,360,360);
+
+    fill(0);
+    ellipse(10,590,340,340);
+
+    fill(255);
+    ellipse(10,590,300,300);
+
+    fill(0);
+    ellipse(10,590,250,250);
+
+    fill(255);
+    ellipse(10,590,200,200);
+
+    fill(0);
+    ellipse(10,590,180,180);
+
+    fill(255);
+    ellipse(10,590,150,150);
+
+    //Bottom Right
+    fill(255);
+    ellipse(300,300,50,500);
+
+    fill(255);
+    ellipse(300,300,500,50);
+
+    fill(0);
+    ellipse(300,300,100,100);
+
+    //Top Left
+    fill(255);
+    ellipse(590,590,580,580);
+
+    fill(0);
+    ellipse(590,590,530,530);
+
+    fill(255);
+    ellipse(590,590,500,500);
+
+    fill(0);
+    ellipse(590,590,480,480);
+
+    fill(255);
+    ellipse(590,590,430,430);
+
+    fill(0);
+    ellipse(590,590,390,390);
+
+    fill(255);
+    ellipse(590,590,360,360);
+
+    fill(0);
+    ellipse(590,590,340,340);
+    
+    fill(255);
+    ellipse(590,590,300,300);
+
+    fill(0);
+    ellipse(590,590,250,250);
+
+    fill(255);
+    ellipse(590,590,200,200);
+
+     fill(0);
+    ellipse(590,590,180,180);
+    
+    fill(255);
+    ellipse(590,590,150,150);
+
+    //Middle Circles
+    fill(255);
+    ellipse(300,300,65,65);
+
+    fill(0);
+    ellipse(300,300,30,30);
+
+    fill(0);
+    ellipse(300,430,90,90);
+
+    fill(255);
+    ellipse(300,430,50,60);
+
+    fill(0);
+    ellipse(300,170,90,90);
+
+    fill(255);
+    ellipse(300,170,50,60);
+
+    fill(0);
+    ellipse(170,300,90,90);
+
+    fill(255);
+    ellipse(170,300,60,50);
+
+    fill(0);
+    ellipse(430,300,90,90);
+
+    fill(255);
+    ellipse(430,300,60,50);
+    
+    
+    
+}
